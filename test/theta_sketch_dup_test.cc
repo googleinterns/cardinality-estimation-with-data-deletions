cbe9a482159d9a261900c9c1980cc8a90b5178cc
Renyi Chen <renyichen@google.com>
2020-06-09 19:15:17 -0400

    add generator for string stream data

--- test/theta_sketch_dup_test.cc
+++ test/theta_sketch_dup_test.cc
@@ -2,7 +2,8 @@
 #include <gtest/gtest.h>
 #include <iomanip>
 #include <fstream>
-#include <random>
+#include <string>
+#include "gen_string.h"
 #include "theta_sketch_dup.hpp"
 
 namespace datasketches {
@@ -26,6 +27,26 @@ TEST(THETA_SKETCH_DUP, TEST_SERIALIZE_UNDERTERMINED_CASE_UDNER_ESTIMATION) {
   auto b=update_theta_sketch_dup::deserialize(serialized_bytes.data(), serialized_bytes.size());
   ASSERT_EQ(a.get_estimate(),b.get_estimate());
   // TODO: elements compare between a and b: maybe overload ASSERT_EQ for class theta_sketch_dup, updata_theta_sketch_dup, compact_theta_sketch_dup
+}
+
+TEST(THETA_SKETCH_DUP, TEST_SERIALIZED_SIZE) {
+  auto a=update_theta_sketch_dup::builder().set_lg_k(8).build();
+  std::ofstream f("data_stream");
+  gen_string gen;
+  for (int i=0; i<10; i++) {
+    std::string str=gen.next();
+    f << str << std::endl;
+    a.update(str);
+  }
+  f.close();
+  auto serialized_bytes = a.serialize();
+  std::ofstream output("data",std::ofstream::out);
+  for (auto e:serialized_bytes) {
+    output << e << " ";
+  }
+  std::cout << "------------------------------------------------------" << std::endl;
+  std::cout << "Estimation of the cardinality is: " << a.get_estimate() << std::endl;
+  std::cout << "------------------------------------------------------" << std::endl;
 }
 
 }
